d9727b15f1673e6904d72258399029a806cc8301
Sam Clements <sam@borntyping.co.uk>
2015-11-24 15:16:58 +0000

    New path

--- roles/stage2-user/files/local/share/zsh/lib/environment.zsh
+++ roles/stage2-user/files/local/share/zsh/lib/environment.zsh
@@ -21,11 +21,17 @@ export datasift="$HOME/Development/datasift"
 export sandbox="$HOME/Development/borntyping-sandbox"
 export src="$HOME/Development/src"
 
+#
+# $PATH
+#
+
 # Add directories to $PATH if they exist
 function add_to_path() { [[ -d $1 ]] && export PATH="$1:$PATH"; }
 
-add_to_path "$datasift/chef/bin"        # Chef scripts
-add_to_path "$HOME/.gem/ruby/2.0.0/bin" # Executables installed by 'gem'
-add_to_path "$HOME/.gem/ruby/2.1.0/bin" # Executables installed by 'gem'
-add_to_path "$HOME/.local/bin"          # User installed executables
-add_to_path "$HOME/.bin"                # Personal scripts
+add_to_path "$HOME/Development/datasift/chef/bin"
+add_to_path "$HOME/.gem/ruby/2.0.0/bin"
+add_to_path "$HOME/.multirust/toolchains/nightly/cargo/bin"
+add_to_path "$HOME/.multirust/toolchains/beta/cargo/bin"
+add_to_path "$HOME/.multirust/toolchains/stable/cargo/bin"
+add_to_path "$HOME/.local/bin"
+add_to_path "$HOME/.bin"
