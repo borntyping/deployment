28e000fe3188ce3fe24075f0089c5b0cf6941941
Sam Clements <sam@borntyping.co.uk>
2015-12-09 09:59:02 +0000

    ChefDK stuff

--- roles/stage2-user/files/local/share/zsh/lib/aliases.zsh
+++ roles/stage2-user/files/local/share/zsh/lib/aliases.zsh
@@ -1,4 +1,4 @@
-#!/bin./zsh
+#!/bin/zsh
 
 alias ls='ls --color=auto'              # Use colors for ls output
 alias grep='grep --color=auto'          # Use colors for grep output
@@ -17,6 +17,8 @@ alias blink1-orange='blink1-tool --id all --rgb 200,150,0'
 alias blink1-green='blink1-tool --id all --rgb 0,150,0'
 
 alias ansible-zshrc='ansible-run -t zshrc; source ~/.zshrc'
+
+alias rac='rubocop --auto-correct'
 
 # Send an alert with the status of the previous command
 function alert() {
