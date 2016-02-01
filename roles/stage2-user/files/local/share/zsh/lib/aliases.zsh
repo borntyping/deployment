8c22f7a2618047050970507563b98f48927a8081
Sam Clements <sam@borntyping.co.uk>
2016-02-01 11:44:00 +0000

    Lots of minor updates

--- roles/stage2-user/files/local/share/zsh/lib/aliases.zsh
+++ roles/stage2-user/files/local/share/zsh/lib/aliases.zsh
@@ -4,14 +4,19 @@ alias -g ...='../..'
 alias -g ....='../../..'
 
 # Color basic commands.
-alias ls='ls --color=auto'              # Use colors for ls output
-alias grep='grep --color=auto'          # Use colors for grep output
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+# ls helpers
+alias ll='ls -l'
+alias la='ls -la'
 
 # Short git helpers.
 alias gd='git diff'
 alias gs='git status'
 alias gc='git commit -v'
 alias gca='git commit -v -a'
+alias cola='git-cola &'
 
 # Set blink to specific colors.
 alias blink1-red='blink1-tool --id all --rgb 250,0,0'
@@ -21,6 +26,9 @@ alias blink1-green='blink1-tool --id all --rgb 0,150,0'
 # Run ansible and reload ZSH.
 alias ansible-zshrc='ansible-run -t zshrc; source ~/.zshrc'
 
+# DataSift/Chef aliases
+alias ce='chef exec'
+alias cebe='chef exec bundle exec'
 alias -g sift-dev='chef exec bundle exec sift'
 
 # Send an alert with the status of the previous command
--- roles/stage2-user/files/local/share/zsh/lib/environment.zsh
+++ roles/stage2-user/files/local/share/zsh/lib/environment.zsh
@@ -30,6 +30,7 @@ function add_to_path() { [[ -d $1 ]] && export PATH="$1:$PATH"; }
 
 add_to_path "$HOME/Development/datasift/chef/bin"
 add_to_path "$HOME/Development/datasift/delivery-common/files/default/bin"
+add_to_path "$HOME/.npm/bin/"
 add_to_path "$HOME/.gem/ruby/2.0.0/bin"
 add_to_path "$HOME/.multirust/toolchains/nightly/cargo/bin"
 add_to_path "$HOME/.multirust/toolchains/beta/cargo/bin"
