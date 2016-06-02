d6bd799bd615839e03d0aabd4a888e88a14ce8b7
Sam Clements <sam@borntyping.co.uk>
2016-06-02 15:24:05 +0100

    Improve file handler
    Former-commit-id: a32cca168a4397744940a257ecf0a20cef31b628

--- roles/workstation/files/terminator/plugins/file_url_handler.py
+++ roles/workstation/files/terminator/plugins/file_url_handler.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import inspect
 import os
 import os.path
 import subprocess
@@ -15,10 +16,21 @@ class FileURLHandler(plugin.URLHandler):
 
     match = r'[a-zA-Z0-9_-/\.]*(:[0-9]*)?'
 
+    def get_cwd(self):
+        """ Return current working directory. """
+        # HACK: Because the current working directory is not available to
+        # plugins, we need to use the inspect module to climb up the stack to
+        # the Terminal object and call get_cwd() from there.
+        for frameinfo in inspect.stack():
+            frameobj = frameinfo[0].f_locals.get('self')
+            if frameobj and frameobj.__class__.__name__ == 'Terminal':
+                return frameobj.get_cwd()
+        return None
+
     def callback(self, url):
         """Open the url in IDEA."""
 
-        path = os.path.abspath(url)
+        path = os.path.join(self.get_cwd(), url)
         print('Opening {} in Intellij IDEA'.format(path))
         subprocess.Popen(['idea', path])
 
