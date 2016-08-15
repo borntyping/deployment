b5177e4aef29c1c9fc07181f5925b95cdf506e64
Sam Clements <sam@borntyping.co.uk>
2016-08-15 12:11:40 +0100

    Update Ruby bin path

--- roles/stage2-user/files/local/share/zsh/lib/path.zsh
+++ roles/stage2-user/files/local/share/zsh/lib/path.zsh
@@ -7,7 +7,7 @@ function add_to_path() { [[ -d $1 ]] && export PATH="$1:$PATH"; }
 add_to_path "${datasift}/chef/bin"
 add_to_path "${datasift}/siftdk/src/bin"
 add_to_path "${HOME}/.npm/bin/"
-add_to_path "${HOME}/.gem/ruby/2.0.0/bin"
+add_to_path "${HOME}/.gem/ruby/2.3.0/bin"
 add_to_path "${HOME}/.multirust/toolchains/nightly/cargo/bin"
 add_to_path "${HOME}/.multirust/toolchains/beta/cargo/bin"
 add_to_path "${HOME}/.multirust/toolchains/stable/cargo/bin"
