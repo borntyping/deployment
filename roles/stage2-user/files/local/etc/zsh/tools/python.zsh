b299c107ed2170b3d0271fdbb51e6dadab6d1951
Sam Clements <sam@borntyping.co.uk>
2017-03-14 10:29:29 +0000

    Add PIPENV_MAX_DEPTH environment variable
    Former-commit-id: b9594cf56dca256d20f8bec6fef4162e3cb52f40

--- roles/stage2-user/files/local/etc/zsh/tools/python.zsh
+++ roles/stage2-user/files/local/etc/zsh/tools/python.zsh
@@ -1,8 +1,9 @@
 #!/bin/zsh
-#
-# Load virtualenvwrapper, and stop virtualenv from modifying the prompt
-#
 
+# Make Pipenv search harder for a Pipfile in a parent directory.
+export PIPENV_MAX_DEPTH=5
+
+# Load virtualenvwrapper, and stop virtualenv from modifying the prompt.
 if [[ -f /usr/share/virtualenvwrapper/virtualenvwrapper.sh ]]; then
     export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
     export VIRTUAL_ENV_DISABLE_PROMPT=1
