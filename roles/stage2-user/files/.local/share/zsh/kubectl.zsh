d914515f5f49859008fffa0f9fe03e1d17cafbbc
Sam Clements <sclements@datto.com>
2021-04-26 11:59:31 +0100

    Hammer oc completion into working
    Former-commit-id: d0c85aa6cd49d94de92079b59b6e538a37d5eebe

--- roles/stage2-user/files/.local/share/zsh/kubectl.zsh
+++ roles/stage2-user/files/.local/share/zsh/kubectl.zsh
@@ -3,18 +3,10 @@
 # https://kubernetes.io/docs/tasks/tools/install-kubectl/#enabling-shell-autocompletion
 #
 
-source <(kubectl completion zsh)
+function kj() { kubectl --output="json" "$@"; }
+function ky() { kubectl --output="yaml" "$@" | bat --language="yaml"; }
 
-alias k='kubectl'
-
-function kj() {
-  kubectl --output="json" "$@"
-}
-
-function ky() {
-  kubectl --output="yaml" "$@" | bat --language="yaml"
-}
-
-compdef k="kubectl"
-compdef kj="kubectl"
-compdef ky="kubectl"
+alias k=kubectl
+#compdef k=kubectl
+#compdef kj=kubectl
+#compdef ky=kubectl
