bad5c56028f3e971dfa7f4d4d26a087620a28f1f
Sam Clements <sam@borntyping.co.uk>
2022-08-31 14:18:06 +0100

    Rename variables used in prompt
    Former-commit-id: 378c7822b5c400b3e53d44483f3e66ae1dfda957

--- roles/stage2-user/files/.local/share/zsh/prompt.zsh
+++ roles/stage2-user/files/.local/share/zsh/prompt.zsh
@@ -7,16 +7,17 @@ autoload -U vcs_info         # Version control information
 autoload -U colors && colors # Adds variables for printing colors
 setopt PROMPT_SUBST          # Allow substitution in PROMPT
 
-prompt_fg="${fg[magenta]}"
-prompt_highlight_fg="${fg[blue]}"
+_src_prompt_fg="${fg[magenta]}"
+_src_prompt_fg_highlight="${fg[blue]}"
+_src_prompt_fg_danger="${fg[red]}"
 
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' check-for-staged-changes true
 zstyle ':vcs_info:*' stagedstr "%{${fg[green]}%}+%{${reset_color}%}"
 zstyle ':vcs_info:*' unstagedstr "%{${fg[yellow]}%}+%{${reset_color}%}"
-zstyle ':vcs_info:*' formats "%{${prompt_fg}%}%b%{${reset_color}%}%c%u%m "
-zstyle ':vcs_info:*' actionformats "%{${prompt_fg}%}%b%{${reset_color}%}%c%u%m|%a "
+zstyle ':vcs_info:*' formats "%{${_src_prompt_fg}%}%b%{${reset_color}%}%c%u%m "
+zstyle ':vcs_info:*' actionformats "%{${_src_prompt_fg}%}%b%{${reset_color}%}%c%u%m|%a "
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-remote
 
 function +vi-git-clean() {
@@ -46,27 +47,26 @@ function +vi-git-remote() {
 }
 
 # Display the current virtualenv when active
-function _src_python_info() {
-  prompt_python=""
-  if [[ -n "$PIPENV_ACTIVE" ]]; then
-    prompt_python="%{${prompt_fg}%}pipenv%{${reset_color}%} "
-  elif [[ -n "$VIRTUAL_ENV" ]]; then
+function _src_precmd_venv() {
+  local virtualenv
+  _src_prompt_venv=""
+  if [[ -n "$VIRTUAL_ENV" ]]; then
     if [[ "$VIRTUAL_ENV" =~ /venv$ || "$VIRTUAL_ENV" =~ /.venv$ ]]; then
-      virtualenv_name="$(basename "$(dirname "$VIRTUAL_ENV")")"
+      virtualenv="$(basename "$(dirname "$VIRTUAL_ENV")")"
     else
-      virtualenv_name="$(basename "$VIRTUAL_ENV")"
+      virtualenv="$(basename "$VIRTUAL_ENV")"
     fi
-    prompt_python="%{${prompt_fg}%}virtualenv:${virtualenv_name}%{${reset_color}%} "
+    _src_prompt_venv="%{${_src_prompt_fg}%}venv:${virtualenv}%{${reset_color}%} "
   fi
 }
 
 # Display the current kubectl context
 # Run 'kubectx -u' to unset the current context
-function _src_kubectl_info() {
+function _src_precmd_kubectl() {
   local config context namespace
 
   config="${KUBECONFIG:-$HOME/.kube/config}"
-  prompt_kubectl=""
+  _src_prompt_kubectl=""
 
   if [[ -f "$config" ]]; then
     if (( $+commands[kubectl] )); then
@@ -78,16 +78,16 @@ function _src_kubectl_info() {
     fi
 
     if [[ -n "$context" ]]; then
-      prompt_kubectl="%{$prompt_highlight_fg%}${context}${namespace}%{$reset_color%} "
+      _src_prompt_kubectl="%{$_src_prompt_fg_highlight%}${context}${namespace}%{$reset_color%} "
     fi
   fi
 }
 
 # Display the current AWS profile when active
-function _src_aws_info() {
-  prompt_aws=""
+function _src_precmd_aws() {
+  _src_prompt_aws=""
   if [[ -n "${AWS_PROFILE}" ]]; then
-    prompt_aws="%{${fg[red]}%}aws:${AWS_PROFILE}%{${reset_color}%} "
+    _src_prompt_aws="%{${_src_prompt_fg_danger}%}${AWS_PROFILE}%{${reset_color}%} "
   fi
 }
 
@@ -95,7 +95,7 @@ function _src_aws_info() {
 # needs to run in `precmd_functions` before which is executed before the
 # prompt is shown, but should be before any functions that display the current
 # working directory.
-function _src_unset_zoxide_result() {
+function _src_precmd_zoxide() {
   unset _zoxide_result
 }
 
@@ -113,7 +113,7 @@ function _src_preexec_title() {
   esac
 }
 
-export precmd_functions=($precmd_functions _src_unset_zoxide_result _src_precmd_title vcs_info _src_python_info _src_kubectl_info _src_aws_info)
+export precmd_functions=($precmd_functions _src_precmd_zoxide _src_precmd_title vcs_info _src_precmd_venv _src_precmd_kubectl _src_precmd_aws)
 export preexec_functions=($preexec_functions _src_preexec_title)
 
 # Assemble the prompt
@@ -125,5 +125,5 @@ export preexec_functions=($preexec_functions _src_preexec_title)
 #
 # The %{ and %} characters are used to stop the prompt from counting invisible
 # characters when calculating the length
-PROMPT="%{$prompt_fg%}$ %n@%m %{$prompt_highlight_fg%}%~%{$reset_color%} \${vcs_info_msg_0_}\${prompt_python}\${prompt_kubectl}\${prompt_aws}
-%{$prompt_fg%}$%{$reset_color%} "
+PROMPT="%{$_src_prompt_fg%}$ %n@%m %{$_src_prompt_fg_highlight%}%~%{$reset_color%} \${vcs_info_msg_0_}\${_src_prompt_venv}\${_src_prompt_kubectl}\${_src_prompt_aws}
+%{$_src_prompt_fg%}$%{$reset_color%} "
