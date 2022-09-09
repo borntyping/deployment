b60839ec0e6f29a7e9954a8242d4c9b25fc3267d
Sam Clements <sam@borntyping.co.uk>
2022-09-09 13:04:50 +0100

    Update kubectl prompt
    Former-commit-id: 5ffab5d68f188ee95276fc1461c2c27fa714eb9e

--- roles/stage2-user/files/.local/share/zsh/kubectl.zsh
+++ roles/stage2-user/files/.local/share/zsh/kubectl.zsh
@@ -1,27 +1,32 @@
 #!/usr/bin/env zsh
 
-function k() {
-    kubectl "$@"
-}
+if [[ -v 'commands[kubectl]' ]]; then
+    # shellcheck disable=SC1090
+    source <(kubectl completion zsh)
 
-function kj() {
-    kubectl --output="json" "$@"
-}
+    function k() {
+        kubectl "$@"
+    }
 
-function ky() {
-    kubectl --output="yaml" "$@" | bat --language="yaml"
-}
+    function kj() {
+        kubectl --output="json" "$@"
+    }
 
-if [[ "$+commands[kubectl-ctx]" -eq 1 ]]; then
+    function ky() {
+        kubectl --output="yaml" "$@" | bat --language="yaml"
+    }
+fi
+
+if [[ -v 'commands[kubectl-ctx]' ]]; then
     alias kubectx="kubectl-ctx"
 fi
 
-if [[ "$+commands[kubectl-ns]" -eq 1 ]]; then
+if [[ -v 'commands[kubectl-ns]' ]]; then
     alias kubens="kubectl-ns"
 fi
 
-# shellcheck disable=SC1090
-source <(kubectl completion zsh)
-compdef k=kubectl
-compdef kj=kubectl
-compdef ky=kubectl
+if [[ -v '_comps[kubectl]' ]]; then
+    compdef k=kubectl
+    compdef kj=kubectl
+    compdef ky=kubectl
+fi
