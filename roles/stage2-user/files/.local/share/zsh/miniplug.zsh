b2c5f89703b380ddcf5fcbd7df4f2e6d8ae9aac0
Sam Clements <sam@borntyping.co.uk>
2023-02-09 11:17:44 +0000

    Update dependencies

--- roles/stage2-user/files/.local/share/zsh/miniplug.zsh
+++ roles/stage2-user/files/.local/share/zsh/miniplug.zsh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71b89243b576d5cd700cc45bdfad09dfcd38ff6e1c959bc43e746d118d28ef50
-size 6937
+oid sha256:381b691109f414eae4165fc1dc3f470b332c68907a85cbbfdc6583213dd4088a
+size 6940
