c0f67bfe30e0c714a0d31135f7852f44fb31620a
Sam Clements <sam@borntyping.co.uk>
2023-06-14 10:53:18 +0100

    Update peru dependencies

--- roles/stage2-user/files/.local/share/zsh/atuin.zsh
+++ roles/stage2-user/files/.local/share/zsh/atuin.zsh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e49c91eafce783624d3ecfb95a861319ea2edfd9c369bc4eb3531f153950ee62
-size 32275
+oid sha256:c458806f92867bafec5e587b8ce52946bec73716694c7c576d4984f242d4cdcc
+size 38513
---
+++ roles/stage3-gui/files/.fonts/readme.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:543b7bd3f865ffc05fdfa006a14d24ffe09f003192dc7d4d231f61e3c5d3b7c3
+size 196
