2761b8026259cf350cb7758d71c00b2ed9d535bf
Sam Clements <sam@borntyping.co.uk>
2023-10-20 15:51:46 +0100

    peru reup
    Former-commit-id: 6d53b89c74551ea5c1fee382115398ce7561c40d

--- roles/stage2-user/files/.local/share/zsh/fixup.zsh
+++ roles/stage2-user/files/.local/share/zsh/fixup.zsh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9773c65477a9b2fdc01f7f9b1e0bb40b49fcc27877f5bd7df77c97f2797478e0
-size 924
+oid sha256:ba5adffd7e1623796ce3d573f14361dde893f0829f3d7e39b4624072daac276b
+size 1055
