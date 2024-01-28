94d94ba91dd9db53ca6cfc648ecbf9b4f0144792
Sam Clements <sam@borntyping.co.uk>
2024-01-28 23:44:10 +0000

    Split ZSH into it's own role

--- roles/home/files/.local/share/zsh/atuin.zsh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fffd7d8750f0a8eb871cf172dce141bdbb25572d57bed4b67267c0b6315184df
-size 42258
--- roles/home/files/.local/share/zsh/fixup.zsh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ba5adffd7e1623796ce3d573f14361dde893f0829f3d7e39b4624072daac276b
-size 1055
