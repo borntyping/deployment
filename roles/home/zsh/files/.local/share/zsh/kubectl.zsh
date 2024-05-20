9c3c2f2559c346412ab8fdcdae549beacba2ad11
Sam Clements <sam@borntyping.co.uk>
2024-05-20 13:35:49 +0100

    Move home roles

---
+++ roles/home/zsh/files/.local/share/zsh/kubectl.zsh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0c2ae3448fff7210bbb3d6d0702e1bdeb2442bba2fa08ae1caf924c26c4a60b0
+size 1901
---
+++ roles/home/zsh/files/.local/share/zsh/miniplug.zsh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:381b691109f414eae4165fc1dc3f470b332c68907a85cbbfdc6583213dd4088a
+size 6940
